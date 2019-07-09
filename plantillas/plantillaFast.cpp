37e746489a412660c6ab6f47b501b8e4ccacc6e0
Enrique Junchaya <52640829+Byakko97@users.noreply.github.com>
2019-07-09 13:42:10 -0500

    Create plantillaFast.cpp

---
+++ plantillas/plantillaFast.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0)
+#define rep(i,a,b) for(int i=int(a); i<int(b); i++)
+#define rip(i,a,b) for(int i=(int)a; i>=(int)b; i--)
+#define pb push_back
+#define mp make_pair
+#define fst first
+#define snd second
+#define sz(x) int(x.size())
+#define all(v) (v).begin(), (v).end()
+#define trace(x) cerr << #x << ": " << x <<endl;
+
+using namespace std;
+
+typedef long long int lli;
+typedef pair<lli, lli> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+int main(){
+    fastio;
+
+    return 0;
+}
