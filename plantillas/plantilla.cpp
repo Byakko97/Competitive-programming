be57bd956cf287b4424e5680fb387a6d8290f2ec
Enrique Junchaya <52640829+Byakko97@users.noreply.github.com>
2019-07-09 13:40:28 -0500

    Create plantilla.cpp

---
+++ plantillas/plantilla.cpp
@@ -0,0 +1,27 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0)
+#define rep(i,a,b) for(int i=int(a); i<int(b); i++)
+#define rip(i,a,b) for(int i=(int)a; i>=(int)b; i--)
+#define pb push_back
+#define mp make_pair
+#define fst first
+#define snd second
+#define sz(x) int(x.size())
+#define all(v) (v).begin(), (v).end()
+#define trace(x) cerr << #x << ": " << x <<endl;
+
+using namespace std;
+
+typedef long long int lli;
+typedef pair<lli, lli> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+int main(){
+    fastio;
+
+    return 0;
+}
