5afb0acfc9838771a5e96947a1acbfe42335d8f0
Enrique Junchaya <52640829+Byakko97@users.noreply.github.com>
2019-07-09 14:30:24 -0500

    Update SegmentTree.cpp

--- Estructuras/SegmentTree.cpp
+++ Estructuras/SegmentTree.cpp
@@ -1,23 +1,23 @@
 struct SegmentTree{
-	int N;
-	int A[2 * MAX_N];
+	int n;
+	int t[N<<1];
 	void build(){
-		for(int i=n-1; i>0; i--){
-			A[i]=f(A[i<<1], A[i<<1 | 1]);
+		rip(i,n-1,1){
+			t[i]=f(t[i<<1], t[i<<1 | 1]);
 		}
 	}
 	void update(int i, int v){
-		for(A[i+=N]=v; i>1; i>>=1){
-			int aux=A[i>>1];
-			A[i>>1]=f(A[i], A[i^1]);
-			if(aux==A[i>>1]) break;
+		for(t[i+=n]=v; i>1; i>>=1){
+			int aux=t[i>>1];
+			t[i>>1]=f(t[i], t[i^1]);
+			if(aux==t[i>>1]) break;
 		}
 	}
 	int que(int l, int r){
 		int ans=neutro;
-		for(l += N, r += N;l<r; l>>=1, r>>=1){
-			if(l & 1) ans=f(ans, st.A[l++]);
-			if(r & 1) ans=f(ans, st.A[--r]);
+		for(l+=n, r+=n;l<r; l>>=1, r>>=1){
+			if(l & 1) ans=f(ans, t[l++]);
+			if(r & 1) ans=f(ans, t[--r]);
 		}
 		return ans;
 	}
