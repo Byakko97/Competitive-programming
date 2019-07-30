698d69c829e38434aba625034934f0ee9eba6499
Enrique Junchaya <52640829+Byakko97@users.noreply.github.com>
2019-07-30 16:16:47 -0500

    Create Antroporama.cpp

---
+++ plantillas/Antroporama.cpp
@@ -0,0 +1,28 @@
+// dile a la jardinera que traigo flores
+ 
+#include <bits/stdc++.h>
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define sz(x) int((x).size())
+#define all(v) (v).begin(),(v).end()
+#define trace(x) cerr << #x << " = " << x << endl
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0)
+#define For(i,a,b) for(int i = int(a); i < int(b); ++i)
+#define roF(i,a,b) for(int i = int(a); i >= int(b); i--)
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+const int N=1e6;
+
+
+int main(){
+	fastio;
+
+	return 0;
+}
