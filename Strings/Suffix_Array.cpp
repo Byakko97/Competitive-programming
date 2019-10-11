c7f416cf61bd695415bcdaf731e0b67997a2f42c
Enrique Junchaya <52640829+Byakko97@users.noreply.github.com>
2019-10-10 23:25:32 -0500

    Update Suffix_Array.cpp

--- Strings/Suffix_Array.cpp
+++ Strings/Suffix_Array.cpp
@@ -2,22 +2,24 @@ string s;
 int SA[N], tempSA[N], RA[N], tempRA[N];
 int phi[N], lcp[N], plcp[N];
 int n;
+int c[N];
 
 void csort(int k){
 	int maxi = max(300,n);
-	int c[maxi]={0};
-	rep(i,0,n) 
+	For(i,0,n)
+		c[i]=0;
+	For(i,0,n) 
 		c[i+k<n ? RA[i+k]:0]++;
-	rep(i,1,maxi)
+	For(i,1,maxi)
 		c[i]+=c[i-1];
-	rip(i,n-1,0)
+	roF(i,n-1,0)
 		tempSA[--c[SA[i]+k<n ? RA[SA[i]+k]:0]]=SA[i];
-	rep(i,0,n)
+	For(i,0,n)
 		SA[i]=tempSA[i];
 }
 
 void build(){
-	rep(i,0,n){
+	For(i,0,n){
 		RA[i]=s[i];
 		SA[i]=i;
 	}
@@ -25,9 +27,9 @@ void build(){
 		csort(k);
 		csort(0);
 		int r = tempRA[SA[0]]=0;
-		rep(i,1,n)
+		For(i,1,n)
 			tempRA[SA[i]] = (RA[SA[i]]==RA[SA[i-1]] && RA[SA[i]+k]==RA[SA[i-1]+k])? r:++r;
-		rep(i,0,n)
+		For(i,0,n)
 			RA[i]=tempRA[i];
 		if(r==n-1) break;		
 	}
@@ -36,9 +38,9 @@ void build(){
 void go(){
 	int k=0;
 	phi[SA[0]]=-1;
-	rep(i,1,n)
+	For(i,1,n)
 		phi[SA[i]] = SA[i-1];
-	rep(i,0,n-1){
+	For(i,0,n-1){
 		if(phi[i]==-1){
 			plcp[i]=0;
 			continue;
@@ -47,6 +49,6 @@ void go(){
 		plcp[i]=k;
 		k=max(k-1,0);		
 	}
-	rep(i,0,n)
+	For(i,0,n)
 		lcp[i]=plcp[SA[i]];
 }
