fedb42da3f3f914f89aad569fb94b3dab58a8b6a
Enrique Junchaya <52640829+Byakko97@users.noreply.github.com>
2019-10-26 22:51:10 -0500

    Update D&C.cpp

--- Dynamic_programming/D&C.cpp
+++ Dynamic_programming/D&C.cpp
@@ -1,23 +1,12 @@
 dp[i][j] = min(0<=k<j){dp[i-1][k]+cost[k][j]}
 
 opt[i][j]<=opt[i][j+1] (Probar por el absurdo)
-
-Ejemplo(Guardians of the Lunatics):
-
-int L;
-lli dp[8005][805];
-lli C[8005];
-
-lli cost(lli i, lli j){
-	return (j-i)*(C[j]-C[i]);
-}
-
+	
 void go(int k, int l, int r, int optl, int optr){
 	if(l>r) return;
-	int opt;
-	int m = (l+r)>>1;
-	rep(i, optl, min(m, optr+1)){
-		lli cur = dp[i][k-1] + cost(i, m);
+	int opt, m = (l+r)>>1;
+	For(i, optl, min(m, optr+1)){
+		ll cur = dp[i][k-1] + cost(i, m);
 		if(cur<dp[m][k]){
 			dp[m][k]=cur;
 			opt=i;
@@ -28,17 +17,9 @@ void go(int k, int l, int r, int optl, int optr){
 }
 
 int main(){
-	fastio;
-	int G; cin>>L>>G;
-	G = min(L, G);
-	lli c;
-	rep(i, 1, L+1){
-		cin>>c;
-		C[i]=C[i-1]+c;
-	}
-	rep(i, 0, L+1) rep(j, 0, G+1) dp[i][j]=1LL<<62;
-	rep(i, 1, L+1) dp[i][1]=C[i]*i;
-	rep(i, 2, G+1) go(i, i, L, i-1, L-1); 
+	For(i, 0, L+1) For(j, 0, G+1) dp[i][j]=1LL<<62;
+	For(i, 1, L+1) dp[i][1]=C[i]*i; //caso base
+	For(i, 2, G+1) go(i, i, L, i-1, L-1); 
 	cout<<dp[L][G]<<endl;
 	return 0;
 }
