f72ee2aece535448b614ed60b55838348c7f583c
Enrique Junchaya <52640829+Byakko97@users.noreply.github.com>
2020-05-24 20:46:33 -0300

    Update HLDes.cpp
    Implemetacion mas corta

--- Estructuras/HLDes.cpp
+++ Estructuras/HLDes.cpp
@@ -1,4 +1,4 @@
-//Implementar con segment tree
+//Ejemplo con maximo valor en arista. Cambios simples en update y queries para que los valores sean en vertices
 
 int gid, tam[N], id[N], p[N], d[N], rt[N];
 vi adj[N];
@@ -7,55 +7,44 @@ int makesz(int v=0, int par=-1, int depth=0){
 	tam[v]=1;
 	p[v]=par;
 	d[v]=depth;
-	rep(i,0,sz(adj[v])){
-		int u=adj[v][i];
-		if(u!=par)
-			tam[v]+=makesz(u, v, depth+1);
-	}
+	for(int u: adj[v]) if(u!=par)
+		tam[v]+=makesz(u,v,depth+1);
 	return tam[v];
 }
 
 void dfs(int v=0, int root=0){
 	id[v]=gid++;
 	rt[v]=root;
-	int bigC=-1;
-	int curt=0;
-	rep(i,0,sz(adj[v])){
-		int u = adj[v][i];
-		if(u!=p[v] && tam[u]>curt){
-			curt=tam[u];
-			bigC=u;
-		}
-	}
-	if(bigC!=-1) dfs(bigC, root);
-	rep(i,0,sz(adj[v])){
-		int u = adj[v][i]; 
-		if(u!=p[v] && u!=bigC)
-			dfs(u, u);
-	}
+	if(sz(adj[v])>1 && adj[v][0]==p[v]) swap(adj[v][0],adj[v][1]);
+	for(auto &u: adj[v]) if(u!=p[v] && tam[u]>tam[adj[v][0]])
+		swap(adj[v][0],u);
+	for(auto u:adj[v]) if(u!=p[v])
+		dfs(u, u==adj[v][0]? root:u);
 }
 
 void hldinit(){
+	gid=0;
 	makesz();
 	dfs();
+	st.init(gid);
 }
 
-void hldupdate(int v, int x){
-	update(id[v], x);
+void hldupdate(int u, int v, int x){
+	if(d[u]<d[v]) swap(u,v); //por ser arista
+	st.upd(id[u],x);
 }
 
 int pathque(int u, int v){
-	int ans=neutro();
+	assert(u!=v);
+	int ans=-1;
 	while(u!=-1){
 		if(rt[u]==rt[v]){
-			int a = id[u];
-			int b = id[v];
-			if(a>b) swap(a,b);
-			ans = f(ans, que(a,b));
+			int a = id[u], b = id[v];
+			ans = max(ans, st.que(min(a,b)+1,max(a,b))); //por ser arista
 			u=-1;
 		}else{
 			if(d[rt[u]]>d[rt[v]]) swap(u,v);
-			ans = f(ans, que(id[rt[v]], id[v]));
+			ans = max(ans, st.que(id[rt[v]], id[v]));
 			v = p[rt[v]];
 		}
 	}
