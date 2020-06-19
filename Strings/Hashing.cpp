856fe2d53cb4c4cb37452f268dd41030beb0adf5
Enrique Junchaya <52640829+Byakko97@users.noreply.github.com>
2020-06-19 00:36:41 -0300

    implementacion de hashing con struct

--- Strings/Hashing.cpp
+++ Strings/Hashing.cpp
@@ -1,24 +1,26 @@
-lli pref[N], pp1[N];
-ull pref2[N], pp2[N];
+ll pp1[N];
+ull pp2[N];
 
 void prec(){
 	pp1[0]=pp2[0]=1;
-	rep(i,1,N){
+	For(i,1,N){
 		pp1[i]=(pp1[i-1]*p)%MOD;
 		pp2[i]=pp2[i-1]*p;
 	}
 }
 
-void calc(){
-	s='#'+s;
-	pref[0]=0;
-	pref2[0]=0;
-	rep(i,1,n+1){
-		pref[i]=(pref[i-1]*p+s[i]-'a')%MOD;
-		pref2[i]=pref2[i-1]*p+s[i]-'a';
+struct Hsh{
+	ll pref[N];
+	ull pref2[N];
+	void compute(string s){
+		s = '#' + s;
+		pref[0]=pref2[0]=0;
+		For(i,1,sz(s)){
+			pref[i] = (pref[i-1]*p+s[i]-'a')%MOD;
+			pref2[i]= pref2[i-1]*p+s[i]-'a';
+		}
 	}
-}
-
-pair<lli, ull> conv(int l, int r){
-	return mp((pref[r]-(pref[l-1]*pp1[r-l+1])%MOD + MOD)%MOD, pref2[r]-pref2[l-1]*pp2[r-l+1]);
-}
+	pair<ll, ull> get(int l, int r){ 
+		return mp((pref[r+1]-(pref[l]*pp1[r-l+1])%MOD+MOD)%MOD,pref2[r+1]-pref2[l]*pp2[r-l+1]);
+	}
+};
