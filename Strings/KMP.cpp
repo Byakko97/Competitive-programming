d28c6e150849bc7d06d41a7849cf5295c81ad396
Byakko97 <enriquejh97@gmail.com>
2021-09-09 11:50:13 -0500

    Added useful comment

--- Strings/KMP.cpp
+++ Strings/KMP.cpp
@@ -1,6 +1,13 @@
-int k=0;
-rep(i, 1, n){
-	while(k>0 && s[i]!=s[k]) k=pi[k-1];
-	if(s[i]==s[k]) k++;
-	pi[i]=k;
+// Remember that pi[i] is the length of the longest (proper) prefix of s[0, i]
+// that is also a (proper) suffix of s[0, i]
+int pi[N];
+
+void kmp(string& s) {
+	int n = sz(s);
+	int k = 0;
+	For(i, 1, n){
+		while(k > 0 && s[i] != s[k]) k = pi[k-1];
+		if(s[i] == s[k]) k++;
+		pi[i] = k;
+	}
 }
