a71f76bd21eda190a2b481e00879db624eda0a2d
Byakko97 <enriquejh97@gmail.com>
2021-09-09 09:47:36 -0500

    Fixed compiler dependency

--- Estructuras/PersistentSegTree.cpp
+++ Estructuras/PersistentSegTree.cpp
@@ -1,5 +1,5 @@
-//Update: add in position
-//Query: sum in range
+// Update: add in position
+// Query: sum in range
 
 struct segTree{
 	vi t = vi(1,0), left = vi(1,0), right = vi(1,0);
@@ -9,27 +9,28 @@ struct segTree{
 		return sz(t)-1;
 	}
 	int init(int* A, int L=0, int R=n){
-		if(L+1==R) return new_node(A[L]);
+		if(L+1 == R) return new_node(A[L]);
 		int M = (L+R)>>1;
-		int l = init(A,L,M), r = init(A,M,R);
-		return new_node(t[l]+t[r],l,r);
+		int l = init(A, L, M), r = init(A, M, R);
+		return new_node(t[l] + t[r], l , r);
 	}
 	int upd(int k, int pos, int v, int L=0, int R=n){
-		int nxt = new_node(t[k],left[k],right[k]);
-		if(L+1==R) t[nxt]+=v;
+		int nxt = new_node(t[k], left[k], right[k]);
+		if(L+1 == R) t[nxt] += v;
 		else{
-			int M = (L+R)>>1;
-			if(pos<M) left[nxt] = upd(left[nxt],pos,v,L,M);
-			else right[nxt] = upd(right[nxt],pos,v,M,R);
+			int M = (L+R) >> 1;
+			int temp;
+			if(pos < M) temp = upd(left[nxt], pos, v, L, M), left[nxt] = temp;
+			else temp = upd(right[nxt], pos, v, M, R), right[nxt] = temp;
 			t[nxt] = t[left[nxt]] + t[right[nxt]];
 		}
 		return nxt;
 	}
 	int que(int k, int l, int r, int L=0, int R=n){
-		if(r<=L || R<=l) return 0;
-		if(l<=L && R<=r) return t[k];
-		int M = (L+R)>>1;
-		return que(left[k],l,r,L,M)+que(right[k],l,r,M,R);
+		if(r <= L || R <= l) return 0;
+		if(l <= L && R <= r) return t[k];
+		int M = (L+R) >> 1;
+		return que(left[k], l, r, L, M)+que(right[k], l, r, M, R);
 	}
 };
 int segTree::n = N;
